463a0491290b6bc9c50dc910598c607046f24b77
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-08 16:15:36 +0300

    fixed main file

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.generate_diff import generate_diff
 
 
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 
-parser.add_argument(
-    '-f',
-    '--format',
-    type=str,
-    help='set format of output'
-)
-args = parser.parse_args()
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        help='set format of output'
+    )
+
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()
