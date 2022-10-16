8f41e95786ee1b78409c9803d000a5ab056a7e9a
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-16 15:41:04 +0300

    fixed function

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,27 +2,30 @@ import json
 import itertools
 
 
+def bool_format(dictionary):
+    for key in dictionary:
+        if dictionary[key] is False:
+            dictionary[key] = 'false'
+        elif dictionary[key] is True:
+            dictionary[key] = 'true'
+
+
 def generate_diff(file1, file2):
     first_file = json.load(open(f'{file1}'))
     second_file = json.load(open(f'{file2}'))
+    bool_format(first_file)
+    bool_format(second_file)
+    keys = first_file.keys() | second_file.keys()
     lines = []
-    for key, value in sorted(first_file.items()):
-        if value is False:
-            value = 'false'
-        if value is True:
-            value = 'true'
-        if key in second_file and first_file[key] == second_file[key]:
-            lines.append(f'    {key}: {value}')
-        elif key in second_file and first_file[key] != second_file[key]:
-            lines.append(f'  - {key}: {value}\n  + {key}: {second_file[key]}')
-        elif key not in second_file:
-            lines.append(f'  - {key}: {value}')
-    for key, value in sorted(second_file.items()):
-        if value is False:
-            value = 'false'
-        if value is True:
-            value = 'true'
-        if key not in first_file:
-            lines.append(f'  + {key}: {value}')
+    for key in sorted(keys):
+        if key not in second_file:
+            lines.append(f'  - {key}: {first_file[key]}')
+        elif key not in first_file:
+            lines.append(f'  + {key}: {second_file[key]}')
+        elif first_file[key] == second_file[key]:
+            lines.append(f'    {key}: {first_file[key]}')
+        else:
+            lines.append(f'  - {key}: {first_file[key]}\n  + '
+                         f'{key}: {second_file[key]}')
     result = itertools.chain('{', lines, '}')
     return '\n'.join(result)
