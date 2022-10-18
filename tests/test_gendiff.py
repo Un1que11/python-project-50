d72d6f1c8634945ed8c6e163e977603701fc56c1
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-18 14:49:42 +0300

    created new tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,5 @@
 from gendiff.generate_diff import generate_diff
+from gendiff.scripts.gendiff import main
 
 
 def test_gendiff_json():
@@ -14,4 +15,22 @@ def test_gendiff_yaml():
         expected = file.read()
 
     assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')\
+           == expected
+
+
+def test_gendiff_tree_json():
+    with open('tests/fixtures/json_tree_result') as file:
+        expected = file.read()
+
+    assert generate_diff('tests/fixtures/file1_tree.json',
+                         'tests/fixtures/file2_tree.json')\
+           == expected
+
+
+def test_gendiff_tree_yml():
+    with open('tests/fixtures/yml_tree_result') as file:
+        expected = file.read()
+
+    assert generate_diff('tests/fixtures/file1_tree.yml',
+                         'tests/fixtures/file2_tree.yml')\
            == expected
