38596d96e736ca32e671f3e488e5cfd12f22d8c7
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-27 20:24:42 +0300

    added new test for json format

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,4 @@
 from gendiff.diff_work.generate_diff import generate_diff
-from gendiff.formatters.plain import plain
 
 
 def test_gendiff_json():
@@ -18,7 +17,7 @@ def test_gendiff_yaml():
            == expected
 
 
-def test_formatter_stylish_json():
+def test_format_stylish_json():
     with open('tests/fixtures/stylish_result.txt') as file:
         expected = file.read()
 
@@ -27,7 +26,7 @@ def test_formatter_stylish_json():
            == expected
 
 
-def test_formatter_stylish_yml():
+def test_format_stylish_yml():
     with open('tests/fixtures/stylish_result.txt') as file:
         expected = file.read()
 
@@ -36,21 +35,31 @@ def test_formatter_stylish_yml():
            == expected
 
 
-def test_formatter_plain_json():
+def test_format_plain_json():
     with open('tests/fixtures/plain_result.txt') as file:
         expected = file.read()
 
     assert generate_diff('tests/fixtures/file1_tree.json',
                          'tests/fixtures/file2_tree.json',
-                         style=plain)\
+                         style='plain')\
            == expected
 
 
-def test_formatter_plain_yml():
+def test_format_plain_yml():
     with open('tests/fixtures/plain_result.txt') as file:
         expected = file.read()
 
     assert generate_diff('tests/fixtures/file1_tree.yml',
                          'tests/fixtures/file2_tree.yml',
-                         style=plain) \
+                         style='plain') \
+           == expected
+
+
+def test_format_json():
+    with open('tests/fixtures/format_json_result.txt') as file:
+        expected = file.read()
+
+    assert generate_diff('tests/fixtures/file1_tree.json',
+                         'tests/fixtures/file2_tree.json',
+                         style='json')\
            == expected
