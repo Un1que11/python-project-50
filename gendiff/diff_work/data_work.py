605017c62e55a58c990e5e8010e9276699b2ca18
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-31 20:00:36 +0300

    created new functions;

--- gendiff/diff_work/data_work.py
+++ gendiff/diff_work/data_work.py
@@ -1,19 +1,28 @@
 from pathlib import Path
 
-import json
-import yaml
 
-
-def open_file(
+def get_data_file(
         file: str
-) -> dict:
-    if Path(file).suffix == '.json':
-        with open(f'{file}') as f:
-            return json.load(f)
+) -> str:
+    with open(f'{file}', 'r') as file_data:
+        return file_data.read()
 
-    elif Path(file).suffix == '.yml' or Path(file).suffix == '.yaml':
-        with open(f'{file}') as f:
-            return yaml.safe_load(f)
+
+def get_file_format(
+        file: str
+) -> str:
+    if Path(file).suffix == '.yml' or Path(file).suffix == '.yaml':
+        return 'yml'
+    elif Path(file).suffix == '.json':
+        return 'json'
+
+
+def get_file_data_and_format(
+    file: str
+) -> tuple:
+    file_data = get_data_file(file)
+    file_format = get_file_format(file)
+    return file_data, file_format
 
 
 def bool_format(
