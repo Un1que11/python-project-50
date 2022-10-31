1d0386e6796ae31042e391d4eb69db3928ecc2a3
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-31 20:01:11 +0300

    created module for argparse

---
+++ gendiff/cli/cli.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def parse_cli_arguments():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+
+    parser.add_argument(
+        'first_file',
+        type=str
+    )
+    parser.add_argument(
+        'second_file',
+        type=str
+    )
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=str,
+        help='set format of output',
+        default='stylish',
+        choices=['stylish', 'plain', 'json']
+    )
+
+    args = parser.parse_args()
+    return args.first_file, args.second_file, args.format
