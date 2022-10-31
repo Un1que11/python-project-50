d63b97a0cc15df6c095435ea78199b3a18d4be49
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-31 19:59:09 +0300

    added parametrize

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,65 +1,61 @@
+import pytest
 from gendiff.diff_work.generate_diff import generate_diff
 
 
-def test_gendiff_json():
-    with open('tests/fixtures/json_result.txt') as file:
+def open_file(file_path):
+    with open(file_path) as file:
         expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')\
-           == expected
+    return expected
 
 
-def test_gendiff_yaml():
-    with open('tests/fixtures/yml_result.txt') as file:
-        expected = file.read()
+@pytest.mark.parametrize(
+    'file_path1, file_path2, test_format, result_path', [
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'stylish',
+            'tests/fixtures/json_result.txt'
+        ),
+        (
+            'tests/fixtures/file1.yml',
+            'tests/fixtures/file2.yml',
+            'stylish',
+            'tests/fixtures/yml_result.txt'
+        ),
+        (
+            'tests/fixtures/file1_tree.json',
+            'tests/fixtures/file2_tree.json',
+            'stylish',
+            'tests/fixtures/stylish_result.txt'
+        ),
+        (
+            'tests/fixtures/file1_tree.yml',
+            'tests/fixtures/file2_tree.yml',
+            'stylish',
+            'tests/fixtures/stylish_result.txt'
+        ),
+        (
+            'tests/fixtures/file1_tree.json',
+            'tests/fixtures/file2_tree.json',
+            'plain',
+            'tests/fixtures/plain_result.txt'
+        ),
+        (
+            'tests/fixtures/file1_tree.yml',
+            'tests/fixtures/file2_tree.yml',
+            'plain',
+            'tests/fixtures/plain_result.txt'
+        ),
+        (
+            'tests/fixtures/file1_tree.json',
+            'tests/fixtures/file2_tree.json',
+            'json',
+            'tests/fixtures/format_json_result.txt'
+        ),
+    ],
+)
+def test_generate_diff(file_path1, file_path2, test_format, result_path):
+    expected = open_file(result_path)
+    test_diff = generate_diff(file_path1, file_path2, test_format)
 
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')\
-           == expected
-
-
-def test_format_stylish_json():
-    with open('tests/fixtures/stylish_result.txt') as file:
-        expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1_tree.json',
-                         'tests/fixtures/file2_tree.json')\
-           == expected
-
-
-def test_format_stylish_yml():
-    with open('tests/fixtures/stylish_result.txt') as file:
-        expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1_tree.yml',
-                         'tests/fixtures/file2_tree.yml')\
-           == expected
-
-
-def test_format_plain_json():
-    with open('tests/fixtures/plain_result.txt') as file:
-        expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1_tree.json',
-                         'tests/fixtures/file2_tree.json',
-                         style='plain')\
-           == expected
-
-
-def test_format_plain_yml():
-    with open('tests/fixtures/plain_result.txt') as file:
-        expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1_tree.yml',
-                         'tests/fixtures/file2_tree.yml',
-                         style='plain') \
-           == expected
-
-
-def test_format_json():
-    with open('tests/fixtures/format_json_result.txt') as file:
-        expected = file.read()
-
-    assert generate_diff('tests/fixtures/file1_tree.json',
-                         'tests/fixtures/file2_tree.json',
-                         style='json')\
-           == expected
+    assert test_diff == expected
