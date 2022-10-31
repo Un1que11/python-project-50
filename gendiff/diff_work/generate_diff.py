dd441a9deeec5f3e8fa60db67ed10443891bc1a6
Mikhail Orlov <crazyminer398@gmail.com>
2022-10-31 20:00:09 +0300

    fixed bugs

--- gendiff/diff_work/generate_diff.py
+++ gendiff/diff_work/generate_diff.py
@@ -1,6 +1,7 @@
 from gendiff.formatters.style_format import style_format
-from gendiff.diff_work.data_work import open_file
+from gendiff.diff_work.data_work import get_file_data_and_format
 from gendiff.diff_work.build_diff import build_diff
+from gendiff.diff_work.parser import parse
 
 
 def generate_diff(
@@ -9,7 +10,11 @@ def generate_diff(
         style='stylish'
 ) -> str:
 
-    first_file = open_file(file1)
-    second_file = open_file(file2)
-    diff = build_diff(first_file, second_file)
+    file1_data, file1_format = get_file_data_and_format(file1)
+    file2_data, file2_format = get_file_data_and_format(file2)
+
+    file1_parse = parse(file1_data, file1_format)
+    file2_parse = parse(file2_data, file2_format)
+
+    diff = build_diff(file1_parse, file2_parse)
     return style_format(diff, style)
